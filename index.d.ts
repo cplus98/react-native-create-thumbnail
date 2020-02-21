9554efa44a7466d209645ad2b8bd9b21e8428602
CoinGoblin07 <coingoblin07@gmail.com>
2020-02-21 14:15:39 +0900

    trimVideo method added

--- index.d.ts
+++ index.d.ts
@@ -1,29 +1,43 @@
 declare module "react-native-video-processing" {
-  export interface Config {
-    url: string;
-    timeStamp?: number;
-    type?: "local" | "remote";
-	format?: "jpeg" | "png";
-	quality?: number;
-	maxWidth?: number;
-	maxHeight?: number;
-	maxDirSize?: number;
-	tolerance?: number;
+	export interface ThumbnailConfig {
+	  url: string;
+	  timeStamp?: number;
+	  type?: "local" | "remote";
+	  format?: "jpeg" | "png";
+	  quality?: number;
+	  maxWidth?: number;
+	  maxHeight?: number;
+	  maxDirSize?: number;
+	  tolerance?: number;
+	}
+  
+	export interface Thumbnail {
+	  path: string;
+	  width: number;
+	  height: number;
+	}
+  
+	const TrimVideo: CreateThumbnail;
+  
+	export interface TrimConfig {
+	  url: string;
+	  startTime?: number;
+	  endTime?: number;
+	  maxDirSize?: number;
+	}
+  
+	export interface Trim {
+	  path: string;
+	}
+  
+	export function createThumbnail(config: ThumbnailConfig): Promise<Thumbnail>;
+	export function trimVideo(config: TrimConfig): Promise<Trim>;
+  
+	export interface CreateThumbnail {
+	  createThumbnail(config: ThumbnailConfig): Promise<Thumbnail>;
+	  trimVideo(config: TrimConfig): Promise<Thumbnail>;
+	}
+	
+	export default CreateThumbnail;
   }
-
-  export interface Thumbnail {
-    path: string;
-    width: number;
-    height: number;
-  }
-
-  export function createThumbnail(config: Config): Promise<Thumbnail>;
-
-  export interface CreateThumbnail {
-    createThumbnail(config: Config): Promise<Thumbnail>;
-  }
-
-  const CreateThumbnail: CreateThumbnail;
-
-  export default CreateThumbnail;
-}
+  
